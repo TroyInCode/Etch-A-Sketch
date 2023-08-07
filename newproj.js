4eb98c49a4de153fc1c6fc0db205b0208570811a
troym20 <legendarytroy20@gmail.com>
2023-08-07 00:05:52 -0400

    Created 16x16 Square Grid

--- newproj.js
+++ newproj.js
@@ -1,4 +1,13 @@
 //Create 16x16 Div Grid Square//
-const square = document.createElement("div");
-square.classList.add("newstyle");
-document.getElementById("container").appendChild(square);
+const container = document.getElementById("container");
+
+function addSquares(squareNum) {
+    for (i = 0; i < squareNum; i++) {
+        let square = document.createElement("div");
+        square.classList.add("grid-item");
+        container.appendChild(square);
+
+    }
+}
+
+addSquares(256);
