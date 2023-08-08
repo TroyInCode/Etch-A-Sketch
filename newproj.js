af85fa21fdf4617313ce5b461bc30ebd3f7914a6
troym20 <legendarytroy20@gmail.com>
2023-08-07 20:27:57 -0400

    Added Event Listeners & CSS

--- newproj.js
+++ newproj.js
@@ -1,6 +1,7 @@
-//Create 16x16 Div Grid Square//
+//Container for Div Squares//
 const container = document.getElementById("container");
 
+//Creates 16x16 Div Grid Sqaures//
 function addSquares(squareNum) {
     for (i = 0; i < squareNum; i++) {
         let square = document.createElement("div");
@@ -11,3 +12,17 @@ function addSquares(squareNum) {
 }
 
 addSquares(256);
+
+//Find All Elements With grid-item Class//
+const allSquares = document.querySelectorAll(".grid-item");
+
+//Event Listeners to Change Colors//
+allSquares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "blue";
+    })
+});
+
+square.addEventListener("mouseout", () => {
+    square.style.backgroundColor = " ";
+});
